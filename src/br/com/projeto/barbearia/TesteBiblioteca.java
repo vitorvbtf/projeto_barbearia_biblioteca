a199d7749e1470a64f08effa3b680238b27221a4
vitorvbtf@gmail.com <101009608+vitorvbtf@users.noreply.github.com>
2023-02-20 14:22:45 -0300

    jar projeto_barbearia_biblioteca

---
+++ src/br/com/projeto/barbearia/TesteBiblioteca.java
@@ -0,0 +1,13 @@
+package br.com.projeto.barbearia;
+
+import br.com.barbearia.projeto_barbearia.modelo.Funcionario;
+import br.com.barbearia.projeto_barbearia.modelo.Gerente;
+
+public class TesteBiblioteca {
+
+	public static void main(String[] args) {
+		Funcionario c = new Gerente();
+
+	}
+
+}
